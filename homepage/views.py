8af48932bb3fa69ed78d593323bb9ef665d7f520
Suzan Dewitz <suzanyaa@yahoo.com>
2023-04-03 12:35:30 +0000

    Updated homepage files and added new files

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Booking
+from .forms import BookingForm
+
+
 
 # Create your views here.
+def home(request):
+    """The view for the start page. Renders the index.html
+    page which also extends the base.html
+    """
+    return render(request, 'index.html')
