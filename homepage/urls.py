3407b2fc7c6fc87bb2e684ba4f26be8d862b07e2
Suzan Dewitz <suzanyaa@yahoo.com>
2023-04-07 15:49:38 +0000

    Add booking.html

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     # ... other URL patterns ...
     path('', views.home, name='home'),
+    path('booking/', views.booking_page, name='booking_page'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Booking
 
 
 # Create your views here.
@@ -7,3 +8,27 @@ def home(request):
     page which also extends the base.html
     """
     return render(request, 'index.html')
+
+
+def booking_page(request):
+    """The view for the booking page. If user is logged in it renders the
+    booking.html, otherwise it renders the page to login or signup.
+    When user has made a booking it redirects to mybooking overview page.
+    It takes the input from the form and store it in the Booking model.
+    """
+    if request.method == 'POST':
+        form = BookingForm(data=request.POST)
+        if form.is_valid():
+            booking_form = form.save(commit=False)
+            booking_form.user = request.user
+            booking_form.save()
+            messages.success(request, 'Booking is confirmed')
+            return redirect('mybookings_page')
+        else:
+            messages.error(
+                request, 'Invalid, incorrect info or double booking')
+    form = BookingForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'booking.html', context)
