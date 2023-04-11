3b4f796ff8b50056b5f14aaf072154f4081f9065
Suzan Dewitz <suzanyaa@yahoo.com>
2023-04-11 20:35:16 +0000

    Add whitenoise

--- tablefinder/settings.py
+++ tablefinder/settings.py
@@ -73,13 +73,14 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+   
 ]
 
 ROOT_URLCONF = 'tablefinder.urls'
