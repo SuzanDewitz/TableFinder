c99f656e36dbd44089fd6c1f8f83beb8e9292fe1
Suzan Dewitz <suzanyaa@yahoo.com>
2023-04-11 21:24:25 +0000

    Add CLOUDINARY_URL to settings.py

--- tablefinder/settings.py
+++ tablefinder/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 from django.contrib.messages import constants as messages
 
-CLOUIDNARY_URL = os.environ.get('CLOUDINARY_URL')
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # credit: CI hello django walkthrough
 if os.path.isfile('env.py'):
