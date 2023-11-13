d4be010edfad18d315931a32930949d112b7c2ad
Suzan Dewitz <suzanyaa@yahoo.com>
2023-11-13 13:18:44 +0000

    Update env.py with new URL

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://itbkfaij:k0GVt8_fDWD4dUPPePDGJCbhWf460dol@flora.db.elephantsql.com/itbkfaij"
+os.environ["SECRET_KEY"] = "django-insecure-9w$q52!h8x69uih1-t)o^gz8z-ygnh7v&a$9dr!y^q)f3gy9s$"
+os.environ["CLOUDINARY_URL"] = "cloudinary://521356626465316:_kmeZ0OV-f-j0Glhe_spJ2ui-1Y@ditfekkky"
+os.environ["DEVELOPMENT"] = "False"
